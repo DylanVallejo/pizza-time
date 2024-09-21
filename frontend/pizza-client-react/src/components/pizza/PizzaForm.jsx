d0ae5283399b656de1049a48c79581c974a0d06f
DylanVallejo <dylan1811vallejo@gmail.com>
2024-09-20 23:13:51 -0500

    - fixed visual presentation for the pizza form

--- frontend/pizza-client-react/src/components/pizza/PizzaForm.jsx
+++ frontend/pizza-client-react/src/components/pizza/PizzaForm.jsx
@@ -33,12 +33,9 @@ export const PizzaForm = () => {
 	}
 
 	return (
-		<form onSubmit={e => handleSubmit(e)}>
-			{/* <input type='text' className="form-control" id='inputTitle' value={title} name='title' onChange={onInputChange}/> */}
-			<div className='container'>
-				{/* <div className='row'> */}
-				<div className='d-flex'>
-					<label>Method</label>
+		<form className='container-fluid' onSubmit={e => handleSubmit(e)}>
+				<div className='d-flex my-2'>
+					<label className='me-1'>Method</label>
 					<select className='col' name="type" value={type} onChange={onInputChange}>
 						<option value="" disabled> Method </option>
 						<option value="CARRY">Carry Out</option>
@@ -46,39 +43,34 @@ export const PizzaForm = () => {
 						<option value="HOME">Delivery</option>
 					</select>	
 				</div>
-				{/* </div> */}
-				<div className='d-flex'>
-					{/* <div  className='col'> */}
-					<label>SIZE:</label>
-					<select  name='size' value={size} onChange={onInputChange}>
-						<option value="" disabled>Size</option>
-						<option value="S">Small</option>
-						<option value="M">Medium</option>
-						<option value="L">Large</option>
-						<option value="XL">Extra Large</option>
-					</select>
-						
+				<div className='d-flex justify-content-start '>
+					<div className=' d-flex col'>
+						<label className='me-1'>SIZE:</label>
+						<select  className='w-100' name='size' value={size} onChange={onInputChange}>
+							<option value="" disabled>Size</option>
+							<option value="S">Small</option>
+							<option value="M">Medium</option>
+							<option value="L">Large</option>
+							<option value="XL">Extra Large</option>
+						</select>
+					</div>
+					<div className='d-flex col mx-3'>
+						<label className='me-1'>Crust: </label>
+						<select className='w-100' name='crust' value={crust} onChange={onInputChange} >
+							<option value="" disabled>Crust</option>
+							<option value="THIN">Thin</option>
+							<option value="NORMAL">Normal</option>
+							<option value="THICK">Thick</option>
+						</select>
+					</div>
+					<div className='d-flex col '>
+						<label className='me-1'>QtY:</label>
+						<input  type='number' className='w-100'/>
+					</div>
 				</div>
-				<div className='d-flex'>
-					<label>Crust: </label>
-					<select name='crust' value={crust} onChange={onInputChange} >
-						<option value="" disabled>Crust</option>
-						<option value="THIN">Thin</option>
-						<option value="NORMAL">Normal</option>
-						<option value="THICK">Thick</option>
-					</select>
-				</div>
-				<div className='d-flex'>
-					<label>QtY:</label>
-					<input  type='number'/>
-				</div>
-			</div>
-					
-			
-			<div className="d-flex justify-content-evenly flex-wrap my-3" style={{  height: 'auto' }}>
+			<div className="d-flex justify-content-evenly flex-wrap my-3 border border-3 border-black" style={{  height: 'auto' }}>
 				{
 					toppings?.map((topic)=>{
-						// console.log(topic.topics_id)
 						return(
 							<div className='d-flex m-1' style={{width: "200px" }}>
 								<input className="me-3" type='checkbox' value={topic.topics_id} name={topic.name} onChange={checksSelected} />
